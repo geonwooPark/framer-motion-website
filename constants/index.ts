ee656cdd086c8d31952c9084ebfdf46562a65da3
박건우 <white0581@naver.com>
2023-12-19 16:00:36 +0900

    Revert "feat: Footer 컴포넌트 제작"
    This reverts commit cb0526949b269f4195addc132560433638bee9e8.

--- constants/index.ts
+++ constants/index.ts
@@ -20,23 +20,3 @@ export const headerListItem = [
     link: '/admin',
   },
 ]
-
-export const footerList = [
-  {
-    _id: 111,
-    title: '문의하기',
-    link: '#',
-    icon: true,
-  },
-  {
-    _id: 112,
-    title: '전자상거래 거래법',
-    link: '#',
-    icon: true,
-  },
-  {
-    _id: 113,
-    title: '개인 정보 정책',
-    link: '#',
-  },
-]
