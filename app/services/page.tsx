0e32478b2d8a1e6106177b6dbd1dc109fd8f2c2c
박건우 <white0581@naver.com>
2023-12-20 13:53:50 +0900

    feat: Services 페이지 제작

--- app/services/page.tsx
+++ app/services/page.tsx
@@ -1,5 +1,44 @@
+import Container from '@/components/Container'
+import FadeIn from '@/components/FadeIn'
+import Title from '@/components/Title'
 import React from 'react'
 
 export default function page() {
-  return <div>page</div>
+  return (
+    <Container>
+      <div className="flex flex-col gap-6 px-4">
+        <FadeIn>
+          <Title title="간단한 리포트 애플리케이션" />
+        </FadeIn>
+        <FadeIn>
+          <div className="flex flex-col items-center gap-8 md:flex-row">
+            <p className="max-w-xl">
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Tenetur
+              inventore, possimus repudiandae unde doloremque voluptates
+              exercitationem in tempora sint autem.
+            </p>
+            <p className="max-w-xl">
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Tenetur
+              inventore, possimus repudiandae unde doloremque voluptates
+              exercitationem in tempora sint autem.
+            </p>
+          </div>
+        </FadeIn>
+        <FadeIn>
+          <div className="flex flex-col items-center gap-8 md:flex-row">
+            <p className="max-w-xl">
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Tenetur
+              inventore, possimus repudiandae unde doloremque voluptates
+              exercitationem in tempora sint autem.
+            </p>
+            <p className="max-w-xl">
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Tenetur
+              inventore, possimus repudiandae unde doloremque voluptates
+              exercitationem in tempora sint autem.
+            </p>
+          </div>
+        </FadeIn>
+      </div>
+    </Container>
+  )
 }
---
+++ components/Title.tsx
@@ -0,0 +1,5 @@
+import React from 'react'
+
+export default function Title({ title }: { title: string }) {
+  return <h2 className="text-2xl font-semibold">{title}</h2>
+}
